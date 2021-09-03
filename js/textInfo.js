f22e42e550a448f3d46b9ae6c4b2a018f3bd5eec
NehalKubade <88080845+NehalKubade@users.noreply.github.com>
2021-09-03 13:25:04 +0530

    Update textInfo.js

--- js/textInfo.js
+++ js/textInfo.js
@@ -1,5 +1,5 @@
-let textareaEl = document.getElementById("text");
-let text = null;
+let textarea = document.getElementById("text");
+
 let data = {
   sentences: 0,
   words: 0,
@@ -14,25 +14,15 @@ let data = {
 const findLength = (item) => (item == null ? 0 : item.length);
 
 const setText = () => {
-  text = textareaEl.value;
-  //  number of new Sentences   text.match(/\056/g)
-  //  number of uppercase       text.match(/[A-Z]/g)
-  //  number of lowercase       text.match(/[a-z]/g)
-  //  number of spaces          text.match(/\s/g)
-  //  number of digits          text.match(/\d/g)
-  //  number of words           text.match(/[a-zA-Z]+/g)
-  //  number of vowels          text.match(/[aeiou]/gi)
-  // numbers of consonant      text.match(/[^aeiou]/gi)
-  if (text != null) {
-    data.sentences = findLength(text.match(/\056/g));
-    data.words = findLength(text.match(/[a-zA-Z]+/g));
-    data.lowercase = findLength(text.match(/[a-z]/g));
-    data.spaces = findLength(text.match(/\s/g));
-    data.digits = findLength(text.match(/\d/g));
-    data.consonants = findLength(text.match(/[^aeiou]/gi));
-    data.uppercase = findLength(text.match(/[A-Z]/g));
-    data.vowels = findLength(text.match(/[aeiou]/gi));
-  }
+  data.sentences = findLength(textarea.value.match(/\056/g));
+  data.words = findLength(textarea.value.match(/[a-zA-Z]+/g));
+  data.lowercase = findLength(textarea.value.match(/[a-z]/g));
+  data.spaces = findLength(textarea.value.match(/\s/g));
+  data.digits = findLength(textarea.value.match(/\d/g));
+  data.consonants = findLength(textarea.value.match(/[^aeiou]/gi));
+  data.uppercase = findLength(textarea.value.match(/[A-Z]/g));
+  data.vowels = findLength(textarea.value.match(/[aeiou]/gi));
+
   localStorage.setItem("data", JSON.stringify(data));
 
   window.location = "info.html";
@@ -43,11 +33,12 @@ const getData = () => {
 };
 
 const showData = () => {
-  let data = getData();
+  let receivedData = getData();
   let htmlContent = "";
-  for (item in data) {
+  for (item in receivedData) {
     htmlContent += `<div class="box">
-        <h2>${data[item]}</h2>
+  
+  <h2>${receivedData[item]}</h2>
         <p>${item}</p>
       </div>`;
   }
